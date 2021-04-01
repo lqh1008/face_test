8a0d82f74dcae2ac9cbc9c385a89e87e8a15277d
lvqihang <lqh1023545902@163.com>
2021-04-01 21:16:45 +0800

    111

--- wwwebpack/src/index.js
+++ wwwebpack/src/index.js
@@ -1,2 +1,7 @@
-// console.log(2222);
-// alert(111)
+import Scaner from './Scaner.js'
+window.LQH_temlateEngine = {
+  render(templateStr, data) {
+    console.log('render函数被调用，我们要命令scaner运行');
+    console.log(123);
+  }
+}
